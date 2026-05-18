5a930d976c791a959ef60cc2dc9d89edcc5d72bf
itrayandra-lab <it.rayandra@gmail.com>
2026-05-18 15:52:59 +0700

    cleanup: remove unused scratch files, orphan views, unused import

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Product;
 use App\Models\Transaction;
-use App\Models\LogAktivitas;
 use Carbon\Carbon;
 
 class DashboardController extends Controller
